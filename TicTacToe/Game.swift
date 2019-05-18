c39c4ccf01f5cfdcebb246e07c89c2abd3d30bbb
ssh <kathishivaram@gmail.com>
2019-05-18 20:33:22 +0530

    add active player to place symbol

---
+++ TicTacToe/Game.swift
@@ -0,0 +1,16 @@
+//
+//  Game.swift
+//  TicTacToe
+//
+//  Created by apple on 5/18/19.
+//  Copyright © 2019 shivaapple. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    var activePlayer = 1  // 1 is cross 2 is nought
+    
+    
+}
--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,6 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var game = Game()
+    @IBOutlet var gameButtons: [UIButton]!
+    
+    
+    @IBAction func gameButtonPressed(_ sender: UIButton) {
+        
+        if game.activePlayer == 1 {
+            sender.setImage(UIImage(named: "cross"), for: [])
+            game.activePlayer = 2
+        }else {
+            sender.setImage(UIImage(named: "nought"), for: [])
+            game.activePlayer = 1
+        }
+        
+        
+    }
+    
     
 }
