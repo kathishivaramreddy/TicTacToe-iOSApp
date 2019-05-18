0394b60cd57c536567fa8d1a5b3069bbaadea8fe
ssh <kathishivaram@gmail.com>
2019-05-18 22:12:53 +0530

    add winner result ui

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -10,17 +10,20 @@ import Foundation
 
 struct Game {
     
+    var activeGame = true
     var activePlayer = 1  // 1 is cross 2 is nought
     var gameState = [0,0,0,0,0,0,0,0,0]
-    var winningCombination = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
+    var winningCombination = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     
     
-    func checkForWinner() {
+    mutating func checkForWinner() -> Int?{
         for combination in winningCombination {
             if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]]  && gameState[combination[1]] == gameState[combination[2]] {
-                
+                activeGame = false
+                return gameState[combination[0]]
             }
         }
+        return nil
     }
     
 }
--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -14,11 +14,12 @@ class ViewController: UIViewController {
     
     @IBOutlet var gameButtons: [UIButton]!
     
+    @IBOutlet weak var winnerLabel: UILabel!
     
     @IBAction func gameButtonPressed(_ sender: UIButton) {
         
         let activePostion = sender.tag - 1
-        if game.gameState[activePostion] == 0 {
+        if game.gameState[activePostion] == 0 && game.activeGame {
             game.gameState[activePostion] = game.activePlayer
             if game.activePlayer == 1 {
                 sender.setImage(UIImage(named: "cross"), for: [])
@@ -32,12 +33,25 @@ class ViewController: UIViewController {
     }
     
     func checkForWinner() {
-        game.checkForWinner()
-        updateViewFromModel()
+        
+        let result = game.checkForWinner()
+        updateViewFromModel(with: result)
     }
     
-    func updateViewFromModel() {
+    func updateViewFromModel(with result: Int?) {
         
+        if !game.activeGame {
+            winnerLabel.isHidden = false
+            if let player = result {
+                winnerLabel.text = player == 1 ? "Cross has won" : "Noughts has won"
+            }else {
+                winnerLabel.text = "Its a draw"
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        winnerLabel.isHidden = true
     }
     
 }
