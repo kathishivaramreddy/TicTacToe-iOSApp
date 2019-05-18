29b8e0cf22ed8134b98fa54b1332d42612891071
ssh <kathishivaram@gmail.com>
2019-05-18 21:46:53 +0530

    add logic for picking winner

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -8,9 +8,19 @@
 
 import Foundation
 
-class Game {
+struct Game {
     
     var activePlayer = 1  // 1 is cross 2 is nought
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    var winningCombination = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
     
+    
+    func checkForWinner() {
+        for combination in winningCombination {
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]]  && gameState[combination[1]] == gameState[combination[2]] {
+                
+            }
+        }
+    }
     
 }
--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -11,21 +11,33 @@ import UIKit
 class ViewController: UIViewController {
     
     var game = Game()
+    
     @IBOutlet var gameButtons: [UIButton]!
     
     
     @IBAction func gameButtonPressed(_ sender: UIButton) {
         
-        if game.activePlayer == 1 {
-            sender.setImage(UIImage(named: "cross"), for: [])
-            game.activePlayer = 2
-        }else {
-            sender.setImage(UIImage(named: "nought"), for: [])
-            game.activePlayer = 1
+        let activePostion = sender.tag - 1
+        if game.gameState[activePostion] == 0 {
+            game.gameState[activePostion] = game.activePlayer
+            if game.activePlayer == 1 {
+                sender.setImage(UIImage(named: "cross"), for: [])
+                game.activePlayer = 2
+                }else {
+                sender.setImage(UIImage(named: "nought"), for: [])
+                game.activePlayer = 1
+            }
         }
-        
-        
+        checkForWinner()
     }
     
+    func checkForWinner() {
+        game.checkForWinner()
+        updateViewFromModel()
+    }
+    
+    func updateViewFromModel() {
+        
+    }
     
 }
